056f24d87b7822e3dad1c14414547aa19e713392
fjsaca2001 <sacafrank2@gmail.com>
2019-04-15 16:46:43 -0500

    Python

---
+++ presentacion2.py
@@ -0,0 +1,10 @@
+""" 
+	Este es mi tercer programa 
+	en el lenguaje python
+	@usuario
+"""
+nombre = input("Ingrese su nombre\n")
+apellido = input("Ingrese su apellido\n")
+# print("%s-%s" % (nombre, apellido))
+# print(nombre,apellido) 
+print("Mi nombre es:",nombre,"\n\t Mi apellido es:\n",apellido)
